c837d70198fb1121ed6da38f6bb63cb692415afc
Sriram <73679247+sriramshiyam@users.noreply.github.com>
2021-09-13 18:50:54 -1200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+const menuBar = document.getElementById("menu-bar");
+const navBar = document.querySelector("header .navbar");
+const navBarLinks = document.querySelectorAll("header .navbar a");
+
+const content = document.querySelector(".home .content");
+const image = document.querySelector(".home .image");
+
+const topleft = document.querySelectorAll(".gallery .topleft");
+const bottomright = document.querySelectorAll(".gallery .bottomright");
+
+let docWidth = document.body.offsetWidth;
+
+let nav = true;
+
+navBarLinks.forEach(x => {
+    x.onclick = () => {
+        navBar.style.height = "0";
+        nav = true;
+    }
+})
+
+menuBar.onclick = () => {
+    if (nav) {
+        navBar.style.height = "auto";
+        nav = false;
+    } else {
+        navBar.style.height = "0";
+        nav = true;
+    }
+}
+
+window.addEventListener('scroll', () => {
+    let width = docWidth > 480 ? 42 : 20;
+    let scrollTop = window.scrollY;
+    let docHeight = document.body.offsetHeight;
+    let winHeight = window.innerHeight;
+    let scrollPercent = scrollTop / (docHeight - winHeight);
+    let scrollPercentRounded = Math.round(scrollPercent * 100);
+
+    content.style.transform = `translateX(-${scrollPercentRounded * 20}px)`;
+    image.style.transform = `translateX(${scrollPercentRounded * 20}px)`;
+    
+    if (scrollPercentRounded >= width) {
+        topleft.forEach(x => {
+            x.style.visibility = "visible";
+            x.style.height = "100%";
+            x.style.width = "100%";
+        });
+        bottomright.forEach(x => {
+            x.style.visibility = "visible";
+            x.style.height = "100%";
+            x.style.width = "100%";
+        });
+    } else {
+        topleft.forEach(x => {
+            x.style.visibility = "hidden";
+            x.style.height = "0";
+            x.style.width = "0";
+        });
+        bottomright.forEach(x => {
+            x.style.visibility = "hidden";
+            x.style.height = "0";
+            x.style.width = "0";
+        });
+    }
+}
+)
